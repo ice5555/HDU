94d0e2fb79f61d2d5b660ff029f2e82281abf123
ice5555 <lhb200251@126.com>
2022-11-23 00:35:58 +0800

    change the code

--- CV/Image_Filtering/image_filtering.py
+++ CV/Image_Filtering/image_filtering.py
@@ -1,14 +1,8 @@
 import cv2
-import random
 import numpy as np
+import random
+import skimage
 import math
-
-
-image = cv2.imread(r"./image/1.jpg")
-cv2.imshow("visualize", image)
-cv2.waitKey(0)
-#print(image)
-
 
 def Gaussian_pyramid(image,level):#高斯金字塔
     temp = image.copy()
@@ -18,92 +12,88 @@ def Gaussian_pyramid(image,level):#高斯金字塔
         dst = cv2.pyrDown(temp)
         pyramid_images.append(dst)
 
-        cv2.imshow("pyramid_down_"+str(i), dst)
+        cv2.imshow("pyramid_down_"+str(i+1), dst)
         cv2.waitKey(0)
         temp = dst.copy()
 
     return pyramid_images[level-1]
-Gaussian_pyramid(image,5)
 
-def gaussian_noise(image, mean=0.1, sigma=0.1):#添加高斯噪声
 
-    image = np.array(image / 255, dtype=np.float32)  
-    #print(image)
-    noise = np.random.normal(mean, sigma, image.shape).astype(dtype=np.float32)
-    gaussian_image = image + noise  
-    gaussian_image = np.clip(gaussian_image, 0, 1)
-    gaussian_image = np.uint8(gaussian_image * 255)
-    cv2.imshow("gaussian_noise:"+str(mean)+" "+str(sigma),gaussian_image)
-    cv2.waitKey(0)
-    return gaussian_image
+def gaussian_noise(image, mean=0.1, sigma=0.001):    # 添加高斯噪声
 
-def salt_pepper_noise(image, prob=0.01):#添加椒盐噪声
+    image = np.array(image / 255, dtype=float)
+    noise = np.random.normal(mean, sigma ** 0.5, image.shape)
+    out = image + noise
+    if out.min() < 0:
+        low_clip = -1.
+    else:
+        low_clip = 0.
+    out = np.clip(out, low_clip, 1.0)
+    out = np.uint8(out * 255)
+    return out
 
-    
-    image = np.array(image / 255, dtype=np.float32)
+def sp_noise(image, prob):    # 添加椒盐噪声
+
+    output = np.zeros(image.shape, np.uint8)
+    thres = 1 - prob
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
-            if random.random() < prob:
-                image[i][j] = 0 if random.random() < 0.5 else 255
+            rdn = random.random()
+            if rdn < prob:
+                output[i][j] = 0
+            elif rdn > thres:
+                output[i][j] = 255
             else:
-                image[i][j] = image[i][j]
-    cv2.imshow("salt_pepper_noise:"+str(prob),image)
-    cv2.waitKey(0)
-    return image
+                output[i][j] = image[i][j]
+    return output
 
 def rmse(image0, image):
     row,col=image0.shape[:2]
-    mse=(np.square(np.subtract(image0,image))/(row*col)).mean()
+    mse=(np.square(np.subtract(image0,image))).mean()
     rmse=math.sqrt(mse)
+    print(rmse)
     return rmse
 
 
-gaussian_image=image
-gaussian_image=gaussian_noise(gaussian_image,mean=0.1, sigma=0.1)#高斯噪声
+image = cv2.imread(r'image/2.jpeg')
 
-print("rmse of gaussian_noise:"+str(rmse(image,gaussian_image)))
 
-salt_pepper_image=image
-salt_pepper_image=salt_pepper_noise(salt_pepper_image,prob=0.01)#椒盐噪声
-print("rmse of salt_pepper_noise:"+str(rmse(image,salt_pepper_image)))
-
-#-----------------------------------------------------------------
-
-Gaussianblur_image = cv2.GaussianBlur(gaussian_image, (9, 9), 1)#高斯平滑
-cv2.imshow("Gaussianblur_gaussian_image",Gaussianblur_image)
+img_sp = sp_noise(image, 0.05)#椒盐
+img_gaussian = gaussian_noise(image, 0.1, 0.03)
+rmse(image, image)
+cv2.imshow('image', image)
+Gaussian_pyramid(image,5)
 cv2.waitKey(0)
-print("rmse of Gaussianblur_gaussian_image:"+str(rmse(image,Gaussianblur_image)))
 
-Medianblur_image=cv2.medianBlur(gaussian_image,3)#中值滤波
-cv2.imshow("Medianblur_gaussian_image",Medianblur_image)
+cv2.imshow('img_sp', img_sp)
+cv2.imshow('img_gaussian', img_gaussian)
 cv2.waitKey(0)
-print("rmse of Medianblur_gaussian_image:"+str(rmse(image,Medianblur_image)))
+rmse(image,img_sp)
+rmse(image,img_gaussian)
 
 
-#-------------------------------------------------------
+img_gaussian_gaussianblur=cv2.GaussianBlur(img_gaussian, (5, 5), 1)#高斯平滑
+img_gaussian_medianblur = cv2.medianBlur(img_gaussian,5)#中值滤波
+rmse(image,img_gaussian_gaussianblur)
+rmse(image,img_gaussian_medianblur)
 
-Gaussianblur_image = cv2.GaussianBlur(salt_pepper_image, (9, 9), 1)#高斯平滑
-cv2.imshow("Gaussianblur_salt_pepper_image",Gaussianblur_image)
+
+
+img_sp_gaussianblur = cv2.GaussianBlur(img_sp,(5, 5), 1)
+img_sp_medianblur=cv2.medianBlur(img_sp,5)
+rmse(image,img_sp_gaussianblur)
+rmse(image,img_sp_medianblur)
+
+cv2.imshow('img_gaussian', img_gaussian)
+cv2.imshow('img_gaussian_gaussianblur', img_gaussian_gaussianblur)
+cv2.imshow('img_gaussian_medianblur', img_gaussian_medianblur)
 cv2.waitKey(0)
-print("rmse of GaussianBlur:"+str(rmse(image,Gaussianblur_image)))
 
-Medianblur_image=cv2.medianBlur(salt_pepper_image,3)#中值滤波
-cv2.imshow("Medianblur_salt_pepper_image",Medianblur_image)
+cv2.imshow('img_sp', img_sp)
+cv2.imshow('img_sp_gaussianblur', img_sp_gaussianblur)
+cv2.imshow('img_sp_medianblur', img_sp_medianblur)
+
+
+
 cv2.waitKey(0)
-print("rmse of Medianblur_salt_pepper_image:"+str(rmse(image,Medianblur_image)))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-cv2.destroyAllWindows()
