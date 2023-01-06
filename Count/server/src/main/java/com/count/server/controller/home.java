192e2bb4ce28ac2d0a30704739ec68dc95f7ca9d
ice5555 <lhb200251@126.com>
2023-01-07 03:41:16 +0800

    后端_webapi_swagger_修改版本之后就可以了TAT

--- Count/server/src/main/java/com/count/server/controller/home.java
+++ Count/server/src/main/java/com/count/server/controller/home.java
@@ -1,18 +1,28 @@
 package com.count.server.controller;
 
-//import org.springframework.http.RequestEntity;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/home")
 public class home {
     @GetMapping("/test1")
-    public ResponseEntity<String> test1(){
-        
-        return ResponseEntity.ok("hello world");
-
+    public ResponseEntity<String> test1(@RequestParam("msg")String msg){
+        return ResponseEntity.ok(msg);
     }
+
+    @PostMapping("/test2")
+    public ResponseEntity<String> test2(@RequestBody String msg){
+        return ResponseEntity.ok(msg);
+    }
+
+
+
+
 }
